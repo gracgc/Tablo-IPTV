8be8c2f8a618c306c5180a36ca664bba3e95d9be
gracgc <61702739+gracgc@users.noreply.github.com>
2020-12-15 19:45:36 +0300

    123
    123

--- my-app/src/components/ForAdmin/TabloEdit/TabloEdit.jsx
+++ my-app/src/components/ForAdmin/TabloEdit/TabloEdit.jsx
@@ -94,7 +94,7 @@ const TabloEdit = (props) => {
             tabloAPI.getServerTime(gameNumber, Date.now()).then(r => {
                 setDif(
                     (r.serverTime - r.runningTime)
-                    + (Math.round((Date.now() - r.localTime))/2)
+                    // + (Math.round((Date.now() - r.localTime))/2)
                 );
                 // console.log(Date.now() - r.localTime)
                 console.log(r.serverTime - r.runningTime)
@@ -141,7 +141,7 @@ const TabloEdit = (props) => {
         tabloAPI.getServerTime(gameNumber, Date.now()).then(r => {
             setDif(
                 (r.serverTime - r.runningTime)
-                + (Math.round((Date.now() - r.localTime))/2)
+                // + (Math.round((Date.now() - r.localTime))/2)
             )
         })
 
