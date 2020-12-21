9fc4735bc8287e84ff529731ed51d6c57759e69e
gracgc <61702739+gracgc@users.noreply.github.com>
2020-12-21 20:44:33 +0300

    123
    123

--- my-app/src/components/ForAdmin/CustomGame/CustomGame.jsx
+++ my-app/src/components/ForAdmin/CustomGame/CustomGame.jsx
@@ -4,20 +4,61 @@ import c1920 from './CustomGame_1920.module.css'
 import {NavLink, withRouter} from "react-router-dom";
 import {reduxForm} from "redux-form";
 import {compose} from "redux";
+import {useSelector} from "react-redux";
+import {getGame, setGameDataAC} from "../../../redux/games_reducer";
+import socket from "../../../socket/socket";
+import {tabloAPI} from "../../../api/api";
+import {getTeams} from "../../../redux/teams_reducer";
 
 
 const CustomGameForm = (props) => {
 
     let width = window.innerWidth;
 
+    const teams = useSelector(
+        state => state.teamsPage.teams
+    );
 
 
+    let [period, setPeriod] = useState();
+
+    const periods = [1, 2, 3, 4]
+
+    const homeTeamGamers = teams.find(t => t.teamType === 'home').gamers;
+
+    const guestsTeamGamers = teams.find(t => t.teamType === 'guests').gamers;
+
+    useEffect(() => {
+        dispatch(getTeams(props.gameNumber));
+    }, []);
+
+    const choosePediod = (period) => {
+        setPeriod(period)
+    }
 
 
     return (
         <div>
             <form onSubmit={props.handleSubmit}>
+                <div className={c.customGameForm}>
+                    <div className={c.customTime}>
+                        <div className={c.periodPanel}>
+                            <div className={c.panelName}>Периоды</div>
+                            {periods.map(p => <span className={period === p ? c.choosenPeriod :c.periods} onClick={(e) => choosePediod(p)}>{p}</span>)}
+                        </div>
+                        <div className={c.timePanel}>
 
+                        </div>
+                    </div>
+                    <div className={c.customGamers}>
+                        <div className={c.teamPanel}>
+
+                        </div>
+                        <div className={c.teamPanel}>
+
+                        </div>
+                    </div>
+                </div>
             </form>
         </div>
     )
@@ -37,12 +78,12 @@ const CustomGame = (props) => {
     };
 
 
-
     return (
         <div className={c.customGame}>
+            <div className={c.menuTitle}>Задайте свои параметры</div>
             <div className={width === 1920 ? c1920.customGamePanel : c.customGamePanel}>
                 <CustomGameReduxForm onSubmit={onSubmit}
-
+                                     gameNumber={gameNumber}
                 />
             </div>
 
