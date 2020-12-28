e5ec5e455ff881f70b5b4e50855994165c42200f
gracgc <61702739+gracgc@users.noreply.github.com>
2020-12-28 22:03:46 +0300

    123123
    1312

--- my-app/src/components/MenuPanel/Video/Video.jsx
+++ my-app/src/components/MenuPanel/Video/Video.jsx
@@ -9,15 +9,14 @@ const Video = (props) => {
 
     let stb_player = window.TvipPlayer
 
-    let mrl1 = "http://str1.iptvportal.ru:8080/britko_2019-06-19--1/index.m3u8";
-    stb_player.playUrl(mrl, "live")
+
 
 
     useEffect(() => {
         if (stb_player !== undefined) {
-            // let mrl = "http://str1.iptvportal.ru:8080/britko_2019-06-19--1/index.m3u8";
-            // let mrl1 = "https://www.hlsplayer.net/#type=m3u8&src=https%3A%2F%2Fbitmovin-a.akamaihd.net%2Fcontent%2Fplayhouse-vr%2Fm3u8s%2F105560.m3u8";
-            // stb_player.playUrl(mrl, "live")
+            let mrl = "http://str1.iptvportal.ru:8080/britko_2019-06-19--1/index.m3u8";
+            let mrl1 = "https://www.hlsplayer.net/#type=m3u8&src=https%3A%2F%2Fbitmovin-a.akamaihd.net%2Fcontent%2Fplayhouse-vr%2Fm3u8s%2F105560.m3u8";
+            stb_player.playUrl(mrl, "live")
         }
     }, [stb_player])
 
