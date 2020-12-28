efe07b6d93b78fd8d8f462dbe9864fbf17b4e2ce
gracgc <61702739+gracgc@users.noreply.github.com>
2020-12-28 21:46:22 +0300

    123
    123

--- my-app/src/components/MenuPanel/Video/Video.jsx
+++ my-app/src/components/MenuPanel/Video/Video.jsx
@@ -1,10 +1,6 @@
 import React, {useEffect, useState} from "react";
 import c from './Video.module.css'
 import c1920 from './Video_1920.module.css'
-import video1 from '../../../content/video/videoplayback.mp4'
-import video2 from '../../../content/video/videoplayback2.mp4'
-import video3 from '../../../content/video/Самое короткое видео на YouTube.mp4'
-import VideoPlayer from 'react-video-js-player';
 
 
 const Video = (props) => {
@@ -13,12 +9,15 @@ const Video = (props) => {
 
     let stb_player = window.TvipPlayer
 
+    let mrl1 = "http://str1.iptvportal.ru:8080/britko_2019-06-19--1/index.m3u8";
+    stb_player.playUrl(mrl, "live")
+
 
     useEffect(() => {
         if (stb_player !== undefined) {
-            let mrl = "http://str1.iptvportal.ru:8080/britko_2019-06-19--1/index.m3u8";
-            let mrl1 = "https://www.hlsplayer.net/#type=m3u8&src=https%3A%2F%2Fbitmovin-a.akamaihd.net%2Fcontent%2Fplayhouse-vr%2Fm3u8s%2F105560.m3u8";
-            stb_player.playUrl(mrl1, "live")
+            // let mrl = "http://str1.iptvportal.ru:8080/britko_2019-06-19--1/index.m3u8";
+            // let mrl1 = "https://www.hlsplayer.net/#type=m3u8&src=https%3A%2F%2Fbitmovin-a.akamaihd.net%2Fcontent%2Fplayhouse-vr%2Fm3u8s%2F105560.m3u8";
+            // stb_player.playUrl(mrl, "live")
         }
     }, [stb_player])
 
@@ -27,7 +26,7 @@ const Video = (props) => {
         <div
             className={width === 1920 ? c1920.video : c.video}
         >
-            {stb_player.playUrl("https://bitmovin-a.akamaihd.net/content/playhouse-vr/m3u8s/105560.m3u8", "live")}
+            
         </div>
     )
 }
