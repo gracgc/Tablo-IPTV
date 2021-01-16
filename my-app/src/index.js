b1f5610da096aa8ac3d762655c55c69c76affd3a
gracgc <61702739+gracgc@users.noreply.github.com>
2021-01-16 16:11:45 +0300

    123

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,7 +5,6 @@ import store from './redux/redux_store';
 
 import 'core-js';
 import 'raf/polyfill';
-import "@babel/polyfill";
 
 import App from "./App";
 import {BrowserRouter} from "react-router-dom";
