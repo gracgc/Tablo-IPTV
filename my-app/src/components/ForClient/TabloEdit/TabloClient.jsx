1c1d41954701c1d62f683f2c4b0e084fe8a4f78f
gracgc <61702739+gracgc@users.noreply.github.com>
2021-01-17 17:19:50 +0300

    123

--- my-app/src/components/ForClient/TabloEdit/TabloClient.jsx
+++ my-app/src/components/ForClient/TabloEdit/TabloClient.jsx
@@ -9,7 +9,6 @@ import socket from "../../../socket/socket";
 import classNames from 'classnames'
 
 
-
 const TabloClient = (props) => {
 
     const dispatch = useDispatch();
@@ -27,68 +26,62 @@ const TabloClient = (props) => {
     }, [props.gameNumber])
 
     let player = window.TvipPlayer;
-    
-    try {
-        player.playUrl("http://str1.iptvportal.ru:8080/britko_2019-06-19--1/index.m3u8", "live")
-    } catch (e) {
-        
-    }
+
+
 
     useEffect(() => {
-        if (preset === 2) {
-            try {
-                player.setVideoWindow(0, 0, 2000, 1000, false)
-            } catch (e) {
-
-            }
+        if (player && preset === 2) {
+            player.playUrl("http://str1.iptvportal.ru:8080/britko_2019-06-19--1/index.m3u8", "live")
+            player.setVideoWindow(0, 0, 1800, 800, false)
         }
     }, [preset])
-
 
 
     return (
         <div className={c.tablo}>
             {preset === 1 &&
-                <div className={c.tablo1}>
-                    <div className={c.time}>
-                        {props.minutesTimer <= 0 ? 0 : props.minutesTimer}:{props.secondsTimer < 10 ? '0' : ''}
-                        {props.secondsTimer <= 0 ? 0 : props.secondsTimer}
-                    </div>
-                    {props.isShowLog ? <div className={c.tempLog}>{props.gameTempLog}</div> : <div className={c.tempLog}></div>}
-                    <div>
+            <div className={c.tablo1}>
+                <div className={c.time}>
+                    {props.minutesTimer <= 0 ? 0 : props.minutesTimer}:{props.secondsTimer < 10 ? '0' : ''}
+                    {props.secondsTimer <= 0 ? 0 : props.secondsTimer}
+                </div>
+                {props.isShowLog ? <div className={c.tempLog}>{props.gameTempLog}</div> :
+                    <div className={c.tempLog}></div>}
+                <div>
 
-                        <div className={props.secondsTimerTimeout < 6 ? c.timeout5sec : c.timeout}>
-                            {(props.timeMemTimerTimeout > 0) &&
-                            `Таймаут ${props.secondsTimerTimeout} секунд`
-                            }
-                        </div>
-                        <div className={c.consLog} >
-                            {props.gameConsLog && props.gameConsLog.map(gcl => gcl.item !== '' && <TabloEventClient key={gcl.id}
-                                                                                                                    item={gcl.item}
-                                                                                                                    id={gcl.id}
-                                                                                                                    teamType={gcl.teamType}
-                                                                                                                    timeMemTimer={props.timeMemTimer}
-                                                                                                                    gameNumber={props.gameNumber}
+                    <div className={props.secondsTimerTimeout < 6 ? c.timeout5sec : c.timeout}>
+                        {(props.timeMemTimerTimeout > 0) &&
+                        `Таймаут ${props.secondsTimerTimeout} секунд`
+                        }
+                    </div>
+                    <div className={c.consLog}>
+                        {props.gameConsLog && props.gameConsLog.map(gcl => gcl.item !== '' &&
+                            <TabloEventClient key={gcl.id}
+                                              item={gcl.item}
+                                              id={gcl.id}
+                                              teamType={gcl.teamType}
+                                              timeMemTimer={props.timeMemTimer}
+                                              gameNumber={props.gameNumber}
                             />)}
-                        </div>
-
                     </div>
-                    <div>
-                        <div className={classNames(c.counter, c.homeTeam)}>
-                            {props.homeCounter} <br/>
-                            {props.homeTeam.name}
-                        </div>
-                        <div className={classNames(c.counter, c.guestsTeam)}>
-                            {props.guestsCounter} <br/>
-                            {props.guestsTeam.name}
-                        </div>
+
+                </div>
+                <div>
+                    <div className={classNames(c.counter, c.homeTeam)}>
+                        {props.homeCounter} <br/>
+                        {props.homeTeam.name}
+                    </div>
+                    <div className={classNames(c.counter, c.guestsTeam)}>
+                        {props.guestsCounter} <br/>
+                        {props.guestsTeam.name}
                     </div>
                 </div>
+            </div>
             }
 
             {preset === 2 &&
             <div>
-                <div  className={c2.tablo2}>
+                <div className={c2.tablo2}>
 
                 </div>
                 <div className={classNames(c2.counter2, c2.homeTeam2)}>
@@ -120,7 +113,7 @@ const TabloClient = (props) => {
                 <div className={c3.teamName}>
                     {props.homeTeam.name}
                 </div>
-                <div  className={c3.gamers}>
+                <div className={c3.gamers}>
                     {props.homeTeam.gamers.map(g => <div>{g.gamerNumber} {g.fullName}</div>)}
                 </div>
 
@@ -131,7 +124,7 @@ const TabloClient = (props) => {
                 <div className={c3.teamName}>
                     {props.guestsTeam.name}
                 </div>
-                <div  className={c3.gamers}>
+                <div className={c3.gamers}>
                     {props.guestsTeam.gamers.map(g => <div>{g.gamerNumber} {g.fullName}</div>)}
                 </div>
             </div>
