bef771a678a7532638b9d502ebb1affc041c032e
gracgc <61702739+gracgc@users.noreply.github.com>
2021-01-26 19:07:13 +0300

    123

--- my-app/src/components/ForClient/TabloEdit/TabloClient.jsx
+++ my-app/src/components/ForClient/TabloEdit/TabloClient.jsx
@@ -44,7 +44,9 @@ const TabloClient = (props) => {
 
     useEffect(() => {
         if (player) {
-            player.playUrl(currentVideo.videoURL, currentVideo.videoType)
+            player.playUrl(currentVideo.videoURL, currentVideo.videoType);
+
+            player2.setVideoWindow(0, 0, 100, 100, true);
             player2.playUrl('https://str1.iptvportal.ru/britko_2019-03-19/video.m3u8', currentVideo.videoType)
         }
     }, [player, currentVideo]);
